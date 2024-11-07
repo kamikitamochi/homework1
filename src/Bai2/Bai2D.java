5276b4b795da2dfaf086a2938b7e173ac13b7246
Dung Le <zealot40@gmail.com>
2024-11-07 19:07:28 +0700

    fix error at Bai2D

--- src/Bai2/Bai2D.java
+++ src/Bai2/Bai2D.java
@@ -1,19 +1,11 @@
 package Bai2;
 
-import java.util.Scanner;
-
 public class Bai2D {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the size of multiplication table: ");
-        int n = sc.nextInt();
-
-        if (n < 1 || n > 10){
-            System.out.println("Invalid size");
-        } else {
-            for (int i = 1; i <= 10; i++) {
-                System.out.println(n + " x " + i + " = " + (n * i));
-            }
+        System.out.println("Multiplication table of 9: ");
+        int n = 9;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
         }
     }
 }
